dc9b0d57bad2e796c8a6fbf5ab82095b006a7ab2
unknown <yopma75@gmail.com>
2023-10-20 15:38:45 +0200

    first commit

---
+++ database/obcms_db.sql
@@ -0,0 +1,139 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 24, 2022 at 08:16 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- Database: `obcms_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbladmin`
+--
+
+CREATE TABLE `tbladmin` (
+  `ID` int(10) NOT NULL,
+  `AdminName` varchar(200) DEFAULT NULL,
+  `UserName` varchar(120) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `Email` varchar(120) DEFAULT NULL,
+  `Password` varchar(200) DEFAULT NULL,
+  `AdminRegdate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbladmin`
+--
+
+INSERT INTO `tbladmin` (`ID`, `AdminName`, `UserName`, `MobileNumber`, `Email`, `Password`, `AdminRegdate`) VALUES
+(1, 'Administrator', 'admin', 9123564987, 'admin@mail.com', '0192023a7bbd73250516f069df18b500', '2023-08-17 02:38:26');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblapplication`
+--
+
+CREATE TABLE `tblapplication` (
+  `ID` int(10) NOT NULL,
+  `UserID` int(5) NOT NULL,
+  `ApplicationID` varchar(200) DEFAULT NULL,
+  `DateofBirth` varchar(200) DEFAULT NULL,
+  `Gender` varchar(50) DEFAULT NULL,
+  `FullName` varchar(200) DEFAULT NULL,
+  `PlaceofBirth` varchar(200) DEFAULT NULL,
+  `NameofFather` varchar(200) DEFAULT NULL,
+  `PermanentAdd` mediumtext DEFAULT NULL,
+  `PostalAdd` mediumtext DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `Dateofapply` timestamp NULL DEFAULT current_timestamp(),
+  `Remark` varchar(200) DEFAULT NULL,
+  `Status` varchar(50) DEFAULT NULL,
+  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tblapplication`
+--
+
+INSERT INTO `tblapplication` (`ID`, `UserID`, `ApplicationID`, `DateofBirth`, `Gender`, `FullName`, `PlaceofBirth`, `NameofFather`, `PermanentAdd`, `PostalAdd`, `MobileNumber`, `Email`, `Dateofapply`, `Remark`, `Status`, `UpdationDate`) VALUES
+(1, 1, '205406252', '1988-06-23', 'Female', 'Samantha Lou Lee', 'Here City', 'Michael Lee', 'Down Here, Here City', '2306', 123654789, 'slee@mail.com', '2022-09-24 05:17:30', 'Sample Remarks', 'Verified', '2022-09-24 05:23:16');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbluser`
+--
+
+CREATE TABLE `tbluser` (
+  `ID` int(10) NOT NULL,
+  `FirstName` varchar(200) DEFAULT NULL,
+  `LastName` varchar(200) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `Address` mediumtext DEFAULT NULL,
+  `Password` varchar(200) NOT NULL,
+  `RegDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbluser`
+--
+
+INSERT INTO `tbluser` (`ID`, `FirstName`, `LastName`, `MobileNumber`, `Address`, `Password`, `RegDate`) VALUES
+(1, 'Mark', 'Cooper', 9123456789, 'Sample Address 101', 'c7162ff89c647f444fcaa5c635dac8c3', '2022-09-24 05:16:50');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblapplication`
+--
+ALTER TABLE `tblapplication`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tbluser`
+--
+ALTER TABLE `tbluser`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tblapplication`
+--
+ALTER TABLE `tblapplication`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbluser`
+--
+ALTER TABLE `tbluser`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
---
+++ user/includes/footer.php
@@ -0,0 +1,14 @@
+ <!-- Footer Start-->
+    <div class="footer-copyright-area">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="footer-copy-right">
+                    <p>Copyright &copy; <?= date("Y") ?> Birth Certificate 
+Services</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer End-->
